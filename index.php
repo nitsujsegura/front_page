c24e5e992394700f30d783600b742f9a87002d5d
nitsujsegura <127389974+nitsujsegura@users.noreply.github.com>
2023-08-29 19:50:27 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,459 @@
+<?php
+include 'DB_connection.php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>His Little Ones Learning Center &mdash;</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" href="pics.png">
+    
+    
+    <link href="https://fonts.googleapis.com/css?family=Muli:300,400,700,900" rel="stylesheet">
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/style.css">
+    
+  </head>
+  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+  
+  <div class="site-wrap">
+
+    <div class="site-mobile-menu site-navbar-target">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div>
+   
+    
+    <header class="site-navbar py-4 js-sticky-header site-navbar-target" role="banner">
+      
+      <div class="container-fluid">
+        <div class="d-flex align-items-center">
+          <div class="mr-auto w-25">
+            <a href="index.php"><img src="pics.png" width="120"></a></div>
+
+          <div class="mx-auto text-center">
+            <nav class="site-navigation position-relative text-right" role="navigation">
+              <ul class="site-menu main-menu js-clone-nav mx-auto d-none d-lg-block  m-0 p-0">
+                <li><a href="#home-section" class="nav-link">Home</a></li>
+                <li><a href="#courses-section" class="nav-link">About Us</a></li>
+                <li><a href="#teachers-section" class="nav-link">Teachers</a></li>
+                <li><a href="#contact-section" class="nav-link">Contact Us</a></li>
+                <li><a href="Registration.php" class="nav-link">Enrollment</a></li>
+              </ul>
+            </nav>
+          </div>
+
+          <div class="ml-auto w-25">
+            <nav class="site-navigation position-relative text-right" role="navigation">
+              <ul class="site-menu main-menu site-menu-dark js-clone-nav mr-auto d-none d-lg-block m-0 p-0">
+                <li class="cta"><a href="login_portal.php" class="nav-link"><span>Login</span></a></li>
+              </ul>
+            </nav>
+            <a href="#" class="d-inline-block d-lg-none site-menu-toggle js-menu-toggle text-black float-right"><span class="icon-menu h3"></span></a>
+          </div>
+        </div>
+      </div>
+      
+    </header>
+
+    <div class="intro-section" id="home-section">
+      
+      <div class="slide-1" style="background-image: url('img/school.png');" data-stellar-background-ratio="0.5">
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-12">
+              <div class="row align-items-center">
+                <div class="col-lg-6 mb-4">
+                  <h1  data-aos="fade-up" data-aos-delay="100">Welcome To His Little Ones Learning Center</h1>
+                  <p class="mb-4"  data-aos="fade-up" data-aos-delay="200">His Little Ones Learning Center is a forerunner of the gospel of Christ. In partnership with Evangelical Christian Baptist Church of Cainta, we value the importance of sharing the gospel of Christ to all our learners. Jesus said, “Let the little children come to me, and do not hinder them, for the kingdom of heaven belongs to such as these.” Matthew 19:14 NIV
+                  </p>
+        
+
+                </div>
+
+             
+              </div>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+
+    
+    <div class="site-section courses-title" id="courses-section">
+      <div class="container">
+        <div class="row mb-5 justify-content-center">
+          <div class="col-lg-7 text-center" data-aos="fade-up" data-aos-delay="">
+            <h2 class="section-title">About Us</h2>
+            <p class="mb-4 text-white"  data-aos="fade-up" data-aos-delay="200">His Little One's Learning Center is a non-profit, non-sectariar, incorporated learning institution, which under and ran by chosen members of Evangelical Christian Baptist Church, the school serves as an educational and evangelistic arm of the church. The school is situated at 2675 G. Ricarte St. Cainta, Rizal and caters to the need of hundereds of average to below-average adn average income families within the community that deserves quality Christian education. His Little One's Learning Center is a non-profit
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="site-section courses-entry-wrap"  data-aos="fade-up" data-aos-delay="100">
+      <div class="container">
+        <div class="row">
+
+        <div class="owl-carousel col-12 nonloop-block-14">
+
+<div class="course bg-white align-self-stretch">
+  <figure class="m-0">
+  <img src="img/hlolcimg3.jpg" alt="Image" class="img-fluid">
+  </figure>
+
+
+</div>
+
+<div class="course bg-white align-self-stretch">
+  <figure class="m-0">
+  <img src="img/img-0361_orig.jpg" alt="Image" class="img-fluid">
+  </figure>
+
+
+</div>
+
+<div class="course bg-white align-self-stretch">
+  <figure class="m-0">
+  <img src="img/dsc-7466_orig.jpg" alt="Image" class="img-fluid">
+  </figure>
+
+
+</div>
+
+<div class="course bg-white align-self-stretch">
+  <figure class="m-0">
+  <img src="img/f10_orig.jpg" alt="Image" class="img-fluid">
+  </figure>
+
+
+</div>
+
+<div class="course bg-white align-self-stretch">
+  <figure class="m-0">
+  <img src="img/img-7183_orig.jpg" alt="Image" class="img-fluid">
+  </figure>
+
+
+</div>
+
+<div class="course bg-white align-self-stretch">
+  <figure class="m-0">
+  <img src="img/img13.jpg" alt="Image" class="img-fluid">
+  </figure>
+
+
+</div>
+
+</div>
+
+         
+
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-7 text-center">
+            <button class="customPrevBtn btn btn-primary m-1">Prev</button>
+            <button class="customNextBtn btn btn-primary m-1">Next</button>
+          </div>
+        </div><br><br>
+        <div class="row mb-5 align-items-center">
+        <div class="col-lg-5 mr-auto order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
+            <h2 class="text-black mb-4">Vision</h2>
+            <p class="mb-4">HLOLC supports families by providing a Christ-centered ample, academically worthy education for children. Daily instruction is given so students may become responsible citizens, be led to receive the Lord Jesus Christ as savior, and live according to what</p>
+
+          
+          </div>
+          
+          <div class="col-lg-5 ml-auto" data-aos="fade-up" data-aos-delay="200">
+            <h2 class="text-black mb-4">Mission</h2>
+            <p class="mb-4">Working in cooperation with parents, HLOLC is to provide a quality, Christian education based on biblical values and principles. The school will make certain that salvation message is presented to and understand by each child.</p>
+          </div>
+      
+       
+      </div>
+    </div>
+    </div>
+    
+    
+
+
+    <div class="site-section" id="programs-section">
+      <div class="container">
+        <div class="row mb-5 justify-content-center">
+          <div class="col-lg-7 text-center"  data-aos="fade-up" data-aos-delay="">
+            <h2 class="section-title">Our Subjects</h2>
+            <p> Filipino </p>
+            <p> English </p>
+            <p> Mathematics </p>
+            <p> ​Christian Living </p>
+            <p> Araling Panlipunan </p>
+            <p> Edukasyon sa Pagpapakatao (EsP) </p>
+            <p> Music, Arts, Physical Education and Health </p>
+            <p> Edukasyong Pantahanan at Pangkabuhayan (EPP) </p>
+            
+          </div> </div>
+          
+          <div class="row mb-5 align-items-center">
+        <div class="col-lg-5 mr-auto order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
+            <h2 class="text-black mb-4">Early Childhood</h2>
+            <p class="mb-4">HLOLC supports families by providing a Christ-centered ample, academically worthy education for children. Daily instruction is given so students may become responsible citizens, be led to receive the Lord Jesus Christ as savior, and live according to what</p>
+
+          
+          </div>
+          
+          <div class="col-lg-5 ml-auto" data-aos="fade-up" data-aos-delay="200">
+            <h2 class="text-black mb-4">Tutorials</h2>
+            <p class="mb-4">Our School also offer tutorials for learners who wants to  advance their lessons in a specific subject matter. It is also an important teaching-learning tool that helps learners strengthen their intellectual, communication and social skills.   ​</p>
+          </div>
+          
+        </div>
+     
+   
+            <h2 class="text-black mb-4 text-center">Elementary</h2>
+            <p class="mb-4">Regarding our Primary School, HLOLC adheres to the  DepEd prescribed K to 12 curriculum. This aims to improve Filipino students' skills in mathematics, science, and linguistics to further exhibit competence in the global job market. With the new curriculum, the Department of Education promises to offer higher quality education through the different strands.​</p>
+          </div>
+     
+   
+  
+    </div>
+     
+       
+    </div>
+
+    <div class="site-section" id="teachers-section">
+      <div class="container">
+
+        <div class="row mb-5 justify-content-center">
+          <div class="col-lg-7 mb-5 text-center"  data-aos="fade-up" data-aos-delay="">
+            <h2 class="section-title">Our Staff</h2>
+            <p class="mb-5">In 1979, His Little Ones Learning Center was envisioned by Rev. Athos Monteclaro and volunteer teachers / members of E.C.B.C. of Cainta. After 21 years of proficiently having a Pre-school, it decided to have its elementary level. </p>
+      
+          </div>
+        </div>
+
+        <div class="row">
+
+          <div class="col-md-6 col-lg-4 mb-4" data-aos="fade-up" data-aos-delay="100">
+            <div class="teacher text-center">
+              <img src="img/image.png" alt="Image" class="img-fluid w-50 rounded-circle mx-auto mb-4">
+              <div class="py-2">
+                <h3 class="text-black">Pastor Ariel </h3>
+                <p class="position">Teacher</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-4 mb-4" data-aos="fade-up" data-aos-delay="200">
+            <div class="teacher text-center">
+              <img src="img/image4" alt="Image" class="img-fluid w-50 rounded-circle mx-auto mb-4">
+              <div class="py-2">
+                <h3 class="text-black">Teacher Vilma</h3>
+                <p class="position">Principal</p>
+               
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-4 mb-4" data-aos="fade-up" data-aos-delay="300">
+            <div class="teacher text-center">
+              <img src="img/image7" alt="Image" class="img-fluid w-50 rounded-circle mx-auto mb-4">
+              <div class="py-2">
+                <h3 class="text-black">Teacher Leah</h3>
+                <p class="position">Teacher</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-4 mb-4" data-aos="fade-up" data-aos-delay="300">
+            <div class="teacher text-center">
+            <img src="img/image9.png" alt="Image" class="img-fluid w-50 rounded-circle mx-auto mb-4">
+              <div class="py-2">
+                <h3 class="text-black">Teacher Annaliza</h3>
+                <p class="position">Teacher</p>
+              </div>
+        </div>
+          </div>
+          <div class="col-md-6 col-lg-4 mb-4" data-aos="fade-up" data-aos-delay="300">
+            <div class="teacher text-center">
+            <img src="img/img8.png" alt="Image" class="img-fluid w-50 rounded-circle mx-auto mb-4">
+              <div class="py-2">
+                <h3 class="text-black">Teacher Christine</h3>
+                <p class="position">Teacher</p>
+              </div>
+        </div>
+          </div>
+          <div class="col-md-6 col-lg-4 mb-4" data-aos="fade-up" data-aos-delay="300">
+            <div class="teacher text-center">
+            <img src="img/img10" alt="Image" class="img-fluid w-50 rounded-circle mx-auto mb-4">
+              <div class="py-2">
+                <h3 class="text-black">Teacher Rommel</h3>
+                <p class="position">Teacher</p>
+              </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section bg-image overlay" style="background-image: url('img/hand.jpg');">
+      <div class="container">
+        <div class="row justify-content-center align-items-center">
+          <div class="col-md-8 text-center testimony">
+            <h3 class="mb-4">Proverbs 22:6 NKJV</h3>
+            <blockquote>
+              <p>&ldquo; Train up a child in the way he should go,
+And when he is old he will not depart from it.
+ &rdquo;</p>
+            </blockquote>
+          </div>
+        </div>
+      </div>
+    </div>
+
+ 
+      
+       
+
+    
+
+
+
+    <div class="site-section bg-light" id="contact-section">
+      <div class="container">
+
+        <div class="row justify-content-center">
+          <div class="col-md-7">
+
+
+            
+            <h2 class="section-title mb-3">Message Us</h2>
+
+          
+            <form method="post"
+    	          action="req/contact.php">
+        		<?php if (isset($_GET['error'])) { ?>
+	    		<div class="alert alert-danger" role="alert">
+				  <?=$_GET['error']?>
+				</div>
+				<?php } ?>
+				<?php if (isset($_GET['success'])) { ?>
+		          <div class="alert alert-success" role="alert">
+		           <?=$_GET['success']?>
+		          </div>
+		        <?php } ?>
+        	<form>
+        	
+			  <div class="mb-3">
+			    <label for="exampleInputEmail1" class="form-label">Email address</label>
+			    <input type="email" class="form-control" placeholder="Email Address" name="email" id="exampleInputEmail1" aria-describedby="emailHelp">
+			    <div id="emailHelp" class="form-text text-light">We'll never share your email with anyone else.</div>
+			  </div>
+			  <div class="mb-3">
+			    <label class="form-label">Full Name</label>
+			    <input type="text" name ="full_name" class="form-control" placeholder="Name">
+			  </div>
+			  <div class="mb-3">
+			    <label class="form-label">Message</label>
+			    <textarea class="form-control"placeholder="Type your Message Here!" name="message" rows="4"></textarea>
+			  </div>
+			  
+
+			   <button type="submit" class="btn btn-primary">Send</button> 	
+			  
+
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+     
+    <footer class="footer-section bg-white">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <h3>About His Little Ones Learning Center</h3>
+            <p>His Little Ones Learning Center, is a nondenominational, Bible-based, community-based Christian School.
+HLOLC is under Department of Education Region IV -A which is also a member of CAMPRISA and RIPRISA​</p>
+          </div>
+
+          <div class="col-md-3 ml-auto">
+            <h3>Links</h3>
+            <ul class="list-unstyled footer-links">
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Courses</a></li>
+              <li><a href="#">Programs</a></li>
+              <li><a href="#">Teachers</a></li>
+            </ul>
+          </div>
+
+          <div class="col-md-4">
+            <h3>Enroll Now</h3>
+            <p>HLOLC is now accepting students'application for SY 2023-2024! Be part of HLOLC Preschool and Elementary Department!</p>
+         
+              <div class="d-flex mb-5">
+                <a href="Registration.php" target=" ">
+					<button type="submit" class="btn btn-primary rounded-0">Join Us</button> </a>
+              </div>
+        
+          </div>
+
+        </div>
+
+        <div class="row pt-5 mt-5 text-center">
+          <div class="col-md-12">
+            <div class="border-top pt-5">
+            <p>
+        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+        Copyright &copy;<script>document.write(new Date().getFullYear());</script> His Little Ones Learning Center | All rights reserved <i  aria-hidden="true"></i> <a href="https://colorlib.com" target="_blank" ></a>
+        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+      </p>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    </footer>
+
+  
+    
+  </div> <!-- .site-wrap -->
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/jquery.countdown.min.js"></script>
+  <script src="js/bootstrap-datepicker.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.fancybox.min.js"></script>
+  <script src="js/jquery.sticky.js"></script>
+
+  
+  <script src="js/main.js"></script>
+    
+  </body>
+</html>
